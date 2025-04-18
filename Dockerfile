9801f26cfc3c918e03d6dc49310afedc4a2d4c1b
Nick <ngaow29@gmail.com>
2025-04-18 13:16:16 -0400

    set up websocket connection for creating/joining room

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+COPY ./frontend /app/frontend
+
 
 #Production Version: CMD ["gunicorn", "--bind", "0.0.0.0:8080", "test:app"] --> later use for deployment
 #current flask only handle 1 thread a time
--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, jsonify, render_template
-import logging
+from flask_socketio import SocketIO
 from logging.handlers import RotatingFileHandler
+
+import logging
 import os
 
-app = Flask(__name__, static_folder="frontend", template_folder="frontend/templates")
+app = Flask(__name__, static_folder="frontend/static", template_folder="frontend/templates")
+socketio = SocketIO(app, cors_allowed_origins="*") #INSECURE only for dev stage
+#later for production
+# socketio = SocketIO(app, cors_allowed_origins=["https://yourdomain.com"])
 
 # (logs/app.log）
 if not os.path.exists("logs"):
@@ -16,6 +21,10 @@ file_handler.setFormatter(logging.Formatter(
 ))
 app.logger.setLevel(logging.INFO)
 app.logger.addHandler(file_handler)
+
+#room handles
+from sockets.room_socket import register_room_handlers
+register_room_handlers(socketio)
 
 
 @app.route("/ping")
@@ -32,11 +41,17 @@ def ping():
 def home():
     return "Hello World!"
 
-
 @app.route("/game")
 def game():
-    return render_template("game.html")
+    room_id = request.args.get("room_id", "UNKNOWN")
+    room_name = request.args.get("room_name", "Untitled Room")
+    return render_template("game.html", room_id=room_id, room_name=room_name)
+
+@app.route("/homepage")
+def homepage():
+    return render_template("homepage.html", rooms=[])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    # app.run(host="0.0.0.0", port=8080)
+    socketio.run(app, host="0.0.0.0", port=8080) #change to socket
