0c0ae88f9df9784cf60471468f8854043b32ce4c
Enhui Dong <enhuidon@buffalo.edu>
2025-04-18 00:42:20 -0400

    Fix the bug that the username that commit is not the correct username and moving thing from the test.py to app.py, adding a folder for backend and let app.py call the method instead having the method in the app.py

---
+++ Back_End/Profile.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for, session,jsonify, flash
+import hashlib
+
+from pymongo.synchronous.database import Database
+
+from Database import UserInfo,MatchHistory
+
+def GetUserInfo():
+    auth_token = request.headers.get("Auth_Token")
+    hash_token = hashlib.sha256(auth_token.encode()).hexdigest()
+    userinfo = UserInfo.find_one({"Auth_Token": hash_token}, {"_id": 0})  # Exclude _id from response
+    return jsonify(userinfo)
+
+def ChangeIcon():
+    MIME_TYPES = {
+        "image/jpeg": ".jpg",
+        "image/gif": ".gif",
+        "image/png": ".png",
+    }
+    auth_token = request.headers.get("Auth_Token")
+    hash_token = hashlib.sha256(auth_token.encode()).hexdigest()
+    userid = UserInfo.find_one({"Auth_Token": hash_token}, {"_id": 0})["Userid"]
+    types = request.headers.get("Content-Type")
+    path = str(userid) + MIME_TYPES[types]
+    with open(path, "rb") as f:
+        f.write(request.data)
+    UserInfo.update_one({"Userid": userid}, {"$set": {"ImgUrl": path}})
+    return jsonify({"ImageUrl": path})
+
+def GetMatch():
+    auth_token = request.headers.get("Auth_Token")
+    hash_token = hashlib.sha256(auth_token.encode()).hexdigest()
+    userid = UserInfo.find_one({"Auth_Token": hash_token}, {"_id": 0})["Userid"]
+    MatchH_History = MatchHistory.find({"Userid": userid}, {"_id": 0})
+    return jsonify({"Match": MatchH_History})
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from Back_End.Profile import GetMatch,GetUserInfo,ChangeIcon
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +8,15 @@ app = Flask(__name__)
 
 def home():
     return "Hello World!"
+
+@app.route("/GetUserInfo",methods = ["GET"])
+def GetUserInfoPath():
+    return GetUserInfo()
+
+@app.route("/ChangeIcon",methods = ["POST"])
+def ChangeIconPath():
+    return ChangeIcon()
+
+@app.route("/GetMatch",methods = ["GET"])
+def GetMatchPath():
+    return GetMatch()
