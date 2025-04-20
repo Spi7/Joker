1adc133de47f837a83401844dc34f1acd33d664d
Nick <ngaow29@gmail.com>
2025-04-19 22:22:34 -0400

    Websocket handle room list currently.
    1) room list should be send to all users when they landed on home page
    2) when a new room is created, all user should be able to see the room
    
    Waiting:
    1) when user login, they should have auth_token
    
    2) when user logout, the auth_token should be removed
    
    3) user should have user_id

--- frontend/static/js/homepage.js
+++ frontend/static/js/homepage.js
@@ -5,6 +5,8 @@ const socket = io(); // automatically uses WebSocket upgrade
 document.addEventListener("DOMContentLoaded", () => {
   const createRoomBtn = document.getElementById("create-room-btn");
 
+  socket.emit("get_all_rooms"); //send a message to backend asking for the room list
+
   createRoomBtn?.addEventListener("click", () => {
     const userId = sessionStorage.getItem("user_id") || "test-user"; // Replace with real session
     const roomName = prompt("Enter room name:");
@@ -17,15 +19,64 @@ document.addEventListener("DOMContentLoaded", () => {
 
     console.log("create_room emitted!");
   });
+  //==============================================================================================================================
 
+  //create a room, redirect user to the room id
   socket.on("room_created", (data) => {
     console.log("Room created!", data);
     const encodedRoomName = encodeURIComponent(data.room_name);
     window.location.href = `/game?room_id=${data.room_id}&room_name=${encodedRoomName}`;
   });
 
+  //broadcast new room to everyone WHEN someone create a new room
+  socket.on("new_room_broadcast", (data) => {
+    const roomlist = document.querySelector(".room-scroll");
+
+    const roomCard = document.createElement("div");
+    roomCard.className = "room-card";
+    roomCard.innerHTML = `
+        <div class="room-info">
+          <p class="room-name">Room Name: ${data.room_name}</p>
+          <p class="room-players">Players: ${data.players.length}/3</p>
+        </div>
+        <a href="/game?${data.room_id}&room_name=${encodeURIComponent(data.room_name)}" class="join-btn">Join</a> 
+       `;
+
+    roomlist.appendChild(roomCard);
+  });
+
+  //broadcast the room list to everyone WHEN on page or REFRESH
+  socket.on("all_rooms", (roomList) => {
+    const roomScroll = document.querySelector(".room-scroll");
+    roomScroll.innerHTML = "";
+
+    roomList.forEach((data) => {
+      const roomCard = document.createElement("div")
+      roomCard.className = "room-card";
+
+      if (data.players.length == 3) {
+        roomCard.style.display = "none"; //hide this room when there are 3 player
+      }
+
+      roomCard.innerHTML = `
+          <div class="room-info">
+            <p class="room-name">Room Name: ${data.room_name}</p>
+            <p class="room-players">Players: ${data.players.length}/3</p>
+          </div>
+          <a href="/game?room_id=${data.room_id}&room_name=${encodeURIComponent(data.room_name)}" class="join-btn">Join</a>
+         `;
+      roomScroll.appendChild(roomCard)
+    })
+  });
+
+  //error redirect / message
   socket.on("error", (err) => {
     console.error("SocketIO Error:", err);
-    alert("Error: " + err.message);
+    if (err.redirect) {
+      window.location.href = err.redirect;
+    }
+    else {
+      alert("Error: " + (err.message || "Unknown Error"));
+    }
   });
 });
--- sockets/room_socket.py
+++ sockets/room_socket.py
@@ -3,32 +3,58 @@ import uuid
 
 from flask import request
 from flask_socketio import emit, join_room
+from Database import RoomCollection
 
 # from database import rooms
 
 
 #{room_id : [user_id,...], ...}
-rooms = {}
+# rooms = {}
 
 def register_room_handlers(socketio):
+
+    #backend for create room
     @socketio.on("create_room")
     def create_room(data):
         user_id = data.get("user_id")
         room_name = data.get("room_name", "Untitled Room")
+
+        # if not auth_token:
+        #     emit("error", {"message": "Missing authentication token. Please login.", "redirect":"/login"}, room=request.sid)
+        #     return
 
         if not user_id:
             emit("error", {"message": "Missing user_id"})
             return
 
         room_id = str(uuid.uuid4())
-        rooms[room_id] = [user_id]
+        RoomCollection.insert_one({
+            "room_id": room_id,
+            "room_name": room_name,
+            "players": [user_id]
+        })
         join_room(room_id)
 
+        #redirect this user into the game room
         emit("room_created", {
             "room_id": room_id,
             "room_name": room_name,
-            "players": rooms[room_id]
+            "players": [user_id]
         }, room=request.sid)
 
+        #Broadcast to everyone else on homepage
+        emit("new_room_broadcast", {
+            "room_id": room_id,
+            "room_name": room_name,
+            "players": [user_id]
+        }, broadcast=True, include_self=False)
 
-    # @socketio.on("get_all_rooms")
+
+    #room-list are connected and consistently stayed in connected
+    @socketio.on("get_all_rooms")
+    def get_all_rooms():
+        all_rooms = list(RoomCollection.find({}, {"_id": 0}))
+
+        # room=request.sid --> this message is only send to the client that made this request
+        # send back to front_end for event all_rooms
+        emit("all_rooms", all_rooms, room=request.sid)
