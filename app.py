8cdf19d6b9d3d3223640d5b04c9faf7fe1e1e062
abbby33 <schen277@buffalo.edu>
2025-04-27 16:44:49 -0400

    app.py changed

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ def profile():
 
 
 
-socketio.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True)
-
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True)
